992674a80a7949ea4c70d2145b7520a5b05d8e46
Pratik Gupte <pratikgupte16@gmail.com>
2020-12-22 18:10:36 +0100

    test main fun

---
+++ tests/testthat/test-fun_check_endemic.R
@@ -0,0 +1,19 @@
+test_that("basic functionality works", {
+
+  # prepare hawaii extent as string
+  hawaii <- "../testdata/extent_hawaii.gpkg"
+
+  # prepare fish range as string
+  fish_range <- "../testdata/example_fish.shp"
+
+  how_endemic <- end_check_endemic(aoi = hawaii,
+                                   utm_epsg_code = 2782,
+                                   buffer_distance_km = 1,
+                                   sp_range = fish_range)
+
+  # check this is a numeric value
+  testthat::expect_true(
+    is.numeric(how_endemic)
+  )
+
+})
