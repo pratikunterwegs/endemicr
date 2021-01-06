1c06d377779c2d1fe448a741c864d47f2de0a5be
Pratik Gupte <pratikgupte16@gmail.com>
2021-01-06 19:41:56 +0100

    simplify and work in crs supplied

--- R/fun_voronoi_area.R
+++ R/fun_voronoi_area.R
@@ -2,10 +2,11 @@
 #' Divide an area into Voronoi polygons
 #'
 #' @param area_of_interest An sf-object showing the area of interest.
+#' Must have a CRS. Voronoi points will be converted to this CRS.
 #' @param region_demarcation_points A list of 2-element
 #' vectors which will be used to construct Voronoi polygons from the
 #' Hawaii buffer. Set to Niihau (main_islands) and Nihoa (NWHI).
-#'
+#' Must be specified in decimal degrees (WGS84 CRS: EPSG code = 4326).
 #' @return An sf-object with the area of interest demarcated into regions.
 #' @export
 demarcate_regions <- function(
@@ -39,21 +40,34 @@ demarcate_regions <- function(
     msg = "voronoi_area: region demarcation points must be 2-element vectors"
   )
 
+  # check for crs
+  assertthat::assert_that(
+    !is.na(sf::st_crs(area_of_interest)),
+    msg = "voronoi_area: area of interest has no CRS"
+  )
+
   # prepare points for voronoi
   voronoi_points <- Reduce(rbind, region_demarcation_points)
   voronoi_points <- sf::st_multipoint(voronoi_points)
+
+  # make sfc
+  voronoi_points <- sf::st_sfc(voronoi_points, crs = 4326)
+
+  # transform to area crs
+  voronoi_points <- sf::st_transform(
+    voronoi_points,
+    crs = sf::st_crs(area_of_interest)
+  )
+
+  # make voronoi
   aoi_voronoi <- sf::st_voronoi(
     voronoi_points,
     sf::st_union(area_of_interest)
   )
-  # convert to sfc
-  aoi_voronoi <- Reduce(c, aoi_voronoi)
-  aoi_voronoi <- sf::st_sfc(aoi_voronoi)
-  # cast to polygon
-  aoi_voronoi <- sf::st_cast(aoi_voronoi, "POLYGON")
 
-  # set voronoi crs
-  sf::st_crs(aoi_voronoi) <- 4326
+  # THIS IS SOME NEW STUFF, A GEOMETRY COLLECTION
+  aoi_voronoi <- sf::st_collection_extract(aoi_voronoi,
+                                           type = "POLYGON")
 
   # get crossed interesections in case there are multiple points
   aoi_regions <- sf::st_intersection(
@@ -62,14 +76,15 @@ demarcate_regions <- function(
   )
 
   # determine which area covers which demarcated region
-  voronoi_points <- sf::st_sfc(voronoi_points, crs = 4326)
   voronoi_points <- sf::st_cast(voronoi_points, "POINT")
   voronoi_points <- data.frame(
     names = names(region_demarcation_points),
     geometry = voronoi_points
   )
+
   # make sf
-  voronoi_points <- sf::st_sf(voronoi_points, crs = 4326)
+  voronoi_points <- sf::st_sf(voronoi_points,
+                              crs = sf::st_crs(aoi_voronoi))
 
   # which area has which name
   name_order <- sf::st_contains(aoi_voronoi, voronoi_points)
@@ -81,7 +96,13 @@ demarcate_regions <- function(
     name = names(region_demarcation_points)[name_order],
     geometry = aoi_regions
   )
-  aoi_regions <- sf::st_sf(aoi_regions, crs = 4326)
+  aoi_regions <- sf::st_sf(aoi_regions,
+                           crs = sf::st_crs(aoi_voronoi))
+
+  # free message
+  message(
+    paste0("crs used is ", sf::st_crs(aoi_regions,)$input)
+  )
 
   # return
   return(list(
