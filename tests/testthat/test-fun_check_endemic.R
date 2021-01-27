ba762e7f24327d914ed10243c80d14141d41d62b
Pratik Gupte <pratikgupte16@gmail.com>
2021-01-27 20:09:11 +0100

    add test for range overlap above 100%

--- tests/testthat/test-fun_check_endemic.R
+++ tests/testthat/test-fun_check_endemic.R
@@ -4,7 +4,7 @@ test_that("basic functionality works", {
   hawaii <- sf::st_read("../testdata/extent_hawaii.gpkg")
 
   # prepare fish range as string
-  fish_range <- sf::st_read("../testdata/example_fish.shp")
+  fish_range <- sf::st_read("../testdata/example_fish/example_fish.shp")
 
   how_endemic <- end_check_endemic(
     aoi = hawaii,
@@ -18,6 +18,12 @@ test_that("basic functionality works", {
     is.data.frame(how_endemic)
   )
 
+  # test range
+  testthat::expect_lte(
+    sum(how_endemic$p_range),
+    1.0
+  )
+
   # expect fail
   testthat::expect_error(
     end_check_endemic(
@@ -26,5 +32,32 @@ test_that("basic functionality works", {
       buffer_distance_km = 1,
       sp_range = "alsogarbage"
     )
+  )
+})
+
+test_that("range proportion within 1", {
+
+  # prepare hawaii extent as string
+  hawaii <- sf::st_read("../testdata/extent_hawaii.gpkg")
+
+  # prepare fish range as string
+  fish_range <- sf::st_read("../testdata/excess_range_fish/data_0.shp")
+
+  how_endemic <- end_check_endemic(
+    aoi = hawaii,
+    utm_epsg_code = 2782,
+    buffer_distance_km = 1,
+    sp_range = fish_range
+  )
+
+  # check this is a numeric value
+  testthat::expect_true(
+    is.data.frame(how_endemic)
+  )
+
+  # test range
+  testthat::expect_lte(
+    sum(how_endemic$p_range),
+    1.0
   )
 })
